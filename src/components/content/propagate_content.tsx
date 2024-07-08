a60ffdc4a1880b8f5e59ad4cdb5b93d8c446d52e
Brian Lam <huy.lam.0054@gmail.com>
2024-07-08 14:29:44 +0700

    1

--- src/components/content/propagate_content.tsx
+++ src/components/content/propagate_content.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { motion } from "framer-motion";
 import { slideInFromTop } from "@/utils/motion";
 import { BannerCard } from "../common/banner_card";
-import { ProjectCard } from "../common/project_card";
+import { StandeeCard } from "../common/standee_card";
 
 export interface IPropagates {}
 
@@ -11,15 +11,15 @@ export function Propagates(props: IPropagates) {
   return (
     <>
       <motion.div
-        className="py-8 px-4 mx-auto max-w-screen-xl md:py-16"
+        className="py-8 px-4 mx-auto max-w-screen-xl md:py-8"
         initial="hidden"
         animate="visible"
       >
         <motion.h2
-          className="pb-10 text-2xl md:text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
+          className="text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-orange-400 pb-10 text-2xl md:text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
           variants={slideInFromTop(0)}
         >
-          Biểu ngữ tuyên truyền
+          Banner tuyên truyền
         </motion.h2>
         {/* Biểu ngữ treo dài */}
         <div className="grid md:grid-cols-1 gap-2">
@@ -80,27 +80,69 @@ export function Propagates(props: IPropagates) {
             description={``}
           />
         </div>
+        <motion.h2
+          className="text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-orange-400 py-10 text-2xl md:text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
+          variants={slideInFromTop(0)}
+        >
+          Standee tuyên truyền
+        </motion.h2>
         {/* Biểu ngữ treo đứng */}
-        <div className="grid md:grid-cols-4 gap-2 mt-8">
-          <ProjectCard
-            type={"Code"}
-            name={
-              "Giải bóng đá dành cho cán bộ Công đoàn, công nhân lao động 2024"
-            }
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
+          <StandeeCard
+            type={"Standee"}
+            name={""}
             delay={0.5}
-            img={"/banner1.jpg"}
-            description={`Static websites are now used to bootstrap lots of websites and are
-          becoming the basis for a variety of tools that even influence both
-          web designers and developers.`}
+            img={"/pictures/Standee/Standee FN-01.png"}
+            description={``}
           />
-          <ProjectCard
-            type={"Code"}
-            name={"Start with Design System"}
+          <StandeeCard
+            type={"Standee"}
+            name={""}
             delay={0.5}
-            img={"/banner1.jpg"}
-            description={`Static websites are now used to bootstrap lots of websites and are
-          becoming the basis for a variety of tools that even influence both
-          web designers and developers.`}
+            img={"/pictures/Standee/Standee FN-02.png"}
+            description={``}
+          />
+          <StandeeCard
+            type={"Standee"}
+            name={""}
+            delay={0.5}
+            img={"/pictures/Standee/Standee FN-03.png"}
+            description={``}
+          />
+          <StandeeCard
+            type={"Standee"}
+            name={""}
+            delay={0.5}
+            img={"/pictures/Standee/Standee FN-04.png"}
+            description={``}
+          />
+          <StandeeCard
+            type={"Standee"}
+            name={""}
+            delay={0.5}
+            img={"/pictures/Standee/Standee FN-05.png"}
+            description={``}
+          />
+          <StandeeCard
+            type={"Standee"}
+            name={""}
+            delay={0.5}
+            img={"/pictures/Standee/Standee FN-06.png"}
+            description={``}
+          />
+          <StandeeCard
+            type={"Standee"}
+            name={""}
+            delay={0.5}
+            img={"/pictures/Standee/Standee FN-07.png"}
+            description={``}
+          />
+          <StandeeCard
+            type={"Standee"}
+            name={""}
+            delay={0.5}
+            img={"/pictures/Standee/Standee FN-08.png"}
+            description={``}
           />
         </div>
       </motion.div>
