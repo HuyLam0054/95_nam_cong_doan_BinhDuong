7667782a23875efe79997b8ca432d6bdc399143f
Brian Lam <huy.lam.0054@gmail.com>
2024-07-15 12:42:05 +0700

    1

--- src/components/content/about/about_hero.tsx
+++ src/components/content/about/about_hero.tsx
@@ -13,18 +13,11 @@ const HeroContent = () => {
       <div className="pt-12 px-1 mx-auto max-w-screen-xl">
         <div className="grid w-full lg:grid-cols-5 gap-1">
           <div className="bg-white dark:bg-gray-900 p-8 lg:p-12 lg:col-span-3">
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              className="flex flex-row items-center justify-center w-full z-[20]"
-            >
+            <div className="flex flex-row items-center justify-center w-full z-[20]">
               <div className="h-full w-full flex flex-col justify-center m-auto text-start">
                 <div className="flex flex-row justify-start flex-wrap"></div>
 
-                <motion.div
-                  variants={slideInFromLeft(0.5)}
-                  className="flex flex-col gap-6 mt-5 lg:my-10 font-bold text-gray-800 dark:text-white max-w-[600px] w-auto h-auto"
-                >
+                <div className="flex flex-col gap-6 mt-5 lg:my-10 font-bold text-gray-800 dark:text-white max-w-[600px] w-auto h-auto">
                   <span>
                     <p className="text-3xl lg:text-4xl font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-red-700 mb-3">
                       {`Nhiệt liệt chào mừng 95 năm Ngày Thành lập Công đoàn Việt Nam`}
@@ -33,11 +26,8 @@ const HeroContent = () => {
                       (28/7/1929 - 28/7/2024)
                     </p>
                   </span>
-                </motion.div>
-                <motion.div
-                  variants={slideInFromTop(0)}
-                  className="w-full h-full justify-center items-center flex lg:hidden mt-3"
-                >
+                </div>
+                <div className="w-full h-full justify-center items-center flex lg:hidden mt-3">
                   <Image
                     src="/banner1.jpg"
                     alt="work icons"
@@ -45,11 +35,8 @@ const HeroContent = () => {
                     width={450}
                     className="rounded-full my-4"
                   />
-                </motion.div>
-                <motion.p
-                  variants={slideInFromLeft(0.8)}
-                  className="text-base lg:text-xl text-gray-700 dark:text-gray-300 mb-5 max-w-[600px]"
-                >
+                </div>
+                <p className="text-base lg:text-xl text-gray-700 dark:text-gray-300 mb-5 max-w-[600px]">
                   Ngày 28 tháng 7 năm 1929, Tổng Công hội đỏ Bắc Kỳ, tiền thân
                   của tổ chức Công đoàn Việt Nam chính thức được thành lập, đánh
                   dấu bước ngoặt lớn, khẳng định sự lớn mạnh của phong trào công
@@ -65,22 +52,15 @@ const HeroContent = () => {
                   Nam đã có những đóng góp quan trọng vào công cuộc đấu tranh
                   giành độc lập dân tộc, giải phóng giai cấp và trong công cuộc
                   xây dựng và phát triển đất nước.
-                </motion.p>
+                </p>
               </div>
-            </motion.div>
+            </div>
           </div>
           <div className="bg-white dark:bg-gray-900 p-2 hidden lg:col-span-2 lg:flex">
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              className="flex flex-row items-center justify-center w-full z-[20]"
-            >
+            <div className="flex flex-row items-center justify-center w-full z-[20]">
               <div className="h-full w-full flex flex-col justify-center m-auto text-start">
                 <div className="flex flex-row justify-start flex-wrap">
-                  <motion.div
-                    variants={slideInFromLeft(1.2)}
-                    className="w-full h-full flex justify-center items-center"
-                  >
+                  <div className="w-full h-full flex justify-center items-center">
                     <Image
                       src="/banner1.jpg"
                       alt="banner"
@@ -88,10 +68,10 @@ const HeroContent = () => {
                       width={650}
                       className="h-full"
                     />
-                  </motion.div>
+                  </div>
                 </div>
               </div>
-            </motion.div>
+            </div>
           </div>
         </div>
       </div>
